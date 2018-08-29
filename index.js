30f1cd954bf975854be4b7b1c548cc23615e23d6
Kibetchirchir <langatchirchir@gmail.com>
2018-08-29 23:23:56 +0300

    debug

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const express= require('express')
-const bosyParser= require('bodyParser')
-const request= require('request')
+const express= require('express');
+const bosyParser= require('bodyParser');
+const request= require('request');
 
-const app = express()
+const app = express();
 
 /*
 allow to process data
  */
 
-app.set('port',(process.env.PORT  || 5000))
+app.set('port',(process.env.PORT  || 5000));
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 /*
@@ -23,19 +23,19 @@ ROUTES
 app.get('/' , function (req, res){
 
 res.send ("HI welcome")
-})
+});
 
 /**
  * FACEBOOK
  */
 
 app.get('/webhook/', function (req, res){
-    if(req.query['hub.verify_token']=="chirchir"){
+    if(req.query['hub.verify_token']==="chirchir"){
         res.send(req.query['hub.verify_token'])
     }
     res.send('wrong token')
-})
+});
 
 app.listen(app.get('port'),function () {
     console.log("running: port")
-})
+});
