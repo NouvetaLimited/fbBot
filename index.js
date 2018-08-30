bae1f78368e90a8c1a9a473736ce74776b18afad
Kibetchirchir <langatchirchir@gmail.com>
2018-08-30 03:47:47 +0300

    debug

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const app = express()
 allow to process data
  */
 
-app.set('port',(process.env.PORT  || 5000))
+app.set('port',(process.env.PORT  || 5000));
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 /*
@@ -22,14 +22,14 @@ ROUTES
 
 app.get('/' , function (req, res){
 
-res.send ("HI welcome")
-})
+res.send ("HI welcome");
+});
 
 /**
  * token
  */
 
-let token = 'EAANb0QutBe8BAJoZCi6LtIRaRdQMsE8nWM2ZC37lWJam85lx4kG0sVRvUIWBjaXYRgZC9QOg8TYf4zaR4oPKnnGMnZBtpkByHIZCioDoeouQJ1n904Q3khQV46pXGwZAUPxymmLJi3B8KK9AnfhQk6IZBd4kZCqZAiZA2YrqLCfxcQeeaelACSBNoa',
+let token = 'EAANb0QutBe8BAJoZCi6LtIRaRdQMsE8nWM2ZC37lWJam85lx4kG0sVRvUIWBjaXYRgZC9QOg8TYf4zaR4oPKnnGMnZBtpkByHIZCioDoeouQJ1n904Q3khQV46pXGwZAUPxymmLJi3B8KK9AnfhQk6IZBd4kZCqZAiZA2YrqLCfxcQeeaelACSBNoa';
 
 /**
  * FACEBOOK
@@ -37,26 +37,26 @@ let token = 'EAANb0QutBe8BAJoZCi6LtIRaRdQMsE8nWM2ZC37lWJam85lx4kG0sVRvUIWBjaXYRg
 
 app.get('/webhook/', function (req, res){
     if(req.query['hub.verify_token']==="chirchir"){
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('wrong token')
-})
+    res.send('wrong token');
+});
 
 app.post('/webhook/', function(req, res){
-     let messaging_events =  req.body.entry[0].messaging
+     let messaging_events =  req.body.entry[0].messaging;
     for (let i = 0; 1 < messaging_events.length; i++){
-         let event = messaging_events[i]
-        let sender = event.sender.id
+         let event = messaging_events[i];
+        let sender = event.sender.id;
         if(event.message && event.message.text){
-             let text = event.message.text()
+             let text = event.message.text();
             sendText(sender,"Text echo: " + text.substring(0,100))
         }
     }
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
 function sendText(sender, text){
-    let messageData = {text: text}
+    let messageData = {text: text};
     request({
         url: "https://graph.facebook.com/v2.6/me/messages",
         qs : {access_token : token},
@@ -71,10 +71,10 @@ function sendText(sender, text){
         } else if (response.body.error) {
             console.log("response body error")
         }
-    })
+    });
 
 }
 
 app.listen(app.get('port'),function () {
-    console.log("running: port")
+    console.log("running: port");
 });
