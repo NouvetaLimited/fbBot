c0695c5fe4811e6a0457854cf45faa7a6b959276
Kibetchirchir <langatchirchir@gmail.com>
2018-08-31 10:08:59 +0300

    the persistent

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const bodyParser= require('body-parser')
 const request= require('request')
 
 const app = express()
+/*
+set the preferences
+*/
+const setPreferencesButton = {
+  type: 'web_url',
+  title: 'Set Gift Preferences',
+  url: `https://ticketsoko.com`,
+  webview_height_ratio: 'tall',
+  messenger_extensions: true,
+};
+
+const changeGiftButton = {
+  type: 'postback',
+  title: 'Change Gift',
+  payload: JSON.stringify({
+    type: 'CHANGE_GIFT',
+  }),
+};
+
+
+
+
 
 /*
 allow to process data
@@ -173,53 +195,13 @@ function sendGenericMessage(sender){
 }
 function sendPersistent(sender){
   let messageData= {
-  "persistent_menu":[
-    {
-      "locale":"default",
-      "composer_input_disabled": true,
-      "call_to_actions":[
-        {
-          "title":"My Account",
-          "type":"nested",
-          "call_to_actions":[
-            {
-              "title":"Pay Bill",
-              "type":"postback",
-              "payload":"PAYBILL_PAYLOAD"
-            },
-            {
-              "title":"History",
-              "type":"postback",
-              "payload":"HISTORY_PAYLOAD"
-            },
-            {
-              "title":"Contact Info",
-              "type":"postback",
-              "payload":"CONTACT_INFO_PAYLOAD"
-            }
-          ]
-        },
-        {
-          "type":"web_url",
-          "title":"Latest News",
-          "url":"http://www.messenger.com/",
-          "webview_height_ratio":"compact"
-        }
-      ]
-    },
-    {
-      "locale":"zh_CN",
-      "composer_input_disabled":false,
-      "call_to_actions":[
-        {
-          "title":"Pay Bill",
-          "type":"postback",
-          "payload":"PAYBILL_PAYLOAD"
-        }
-      ]
-    }
-  ]
-}
+    setting_type: 'call_to_actions',
+      thread_state: 'existing_thread',
+      call_to_actions: [
+        setPreferencesButton,
+        changeGiftButton,
+      ],
+  }
 sendRequest(sender, messageData)
 
 }
