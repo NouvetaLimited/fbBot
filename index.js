88ec41f172ab13da6e51b22111dfd83e364925a8
Kibetchirchir <langatchirchir@gmail.com>
2018-09-25 09:51:07 +0300

    dfefg

--- index.js
+++ index.js
@@ -255,17 +255,15 @@ function decideMessage(sender, text1){
                const lee= response.data.status
                console.log(response);
                console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',data,lee);
+               if( lee === 200 ){
+                 sendText(sender,"Account created to activate load Ksh 100 to your account which you will receive on your phone")
+               }else {
+                  sendText(sender,"Wrong OTP. Contact our customer care for assistant")
+               }
              })
              .catch(function (error) {
                console.log(error);
              });
-             if(data === 200 ){
-               sendText(sender,"Account created to activate load Ksh 100 to your account which you will receive on your phone")
-             }else {
-               {
-                sendText(sender,"Wrong OTP. Contact our customer care for assistant")
-               }
-             }
           }
           else{
               sendText(sender,text)
