55d98932e78d80fa1be2b8254b7a544aa74d40ed
Kibetchirchir <langatchirchir@gmail.com>
2018-10-08 10:21:21 +0300

    Debug

--- app.js
+++ app.js
@@ -372,7 +372,7 @@ function decideMessage(sender, text1){
               const data= response.status
               console.log(response);
               const name = response.data.first_name
-              sendText(sender,"Thanks "+name+", your details check out. Our records show you have 2 accounts as below:\n 1:010****12000 \n2:10****1320\nPlease advise which one you’d like to get statement. If all, enter ALL in the space provided if the first enter 1. Please note this is a chargeable service")
+              sendText(sender,"Thanks "+name+", your details check out. Our records show you have 2 accounts as below:\n 1:010****12000 \n2:10****1320\nPlease advise which one you’d like to get statement If all, enter ALL in the space provided if the first enter 1. Please note this is a chargeable service")
             })
             .catch(function (error) {
               console.log(error);
@@ -408,41 +408,7 @@ function decideMessage(sender, text1){
       myServices(sender,"Select a service below")
     }
     else if(text.includes("send money")){
-      axios.get(`https://nouveta.tech/fbbot_BE/public/index.php/api/confirm/${sender}`)
-       .then(function (response) {
-         const data= response.status
-         console.log(response);
-         let status = response.data.status
-         //The person who has an account
-         if(status == '200'){
-           axios.get(`https://graph.facebook.com/${sender}?fields=first_name,last_name,profile_pic&access_token=${token}`)
-            .then(function (response) {
-              const data= response.status
-              console.log(response);
-              const name = response.data.first_name
-              sendText(sender,"Thanks "+name+", your details check out. Our records show you have 2 accounts as below:\n 1:010****12000 \n2:10****1320\nPlease advise which one you’d like to get statement. If all, enter ALL in the space provided if the first enter 1. Please note this is a chargeable service")
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-            axios.get(`https://nouveta.tech/fbbot_BE/public/index.php/api/postmessage/${sender}/statement/${text}`)
-             .then(function (response) {
-               const data= response.status
-               console.log(response);
-             })
-             .catch(function (error) {
-               console.log(error);
-             });
-         }else{
-           sendText(sender, "😞 Sorry notice this was your first time here. Kindly provide me with your id Number")
-           axios.get(`https://nouveta.tech/fbbot_BE/public/index.php/api/postmessage/${sender}/idst/${text}`)
-            .then(function (response) {
-              const data= response.status
-              console.log(response);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+
     }
 
     // The checking in database for past message
