61e3e9ac332635957c84d6e88191c5c1bb79ac4b
Kibetchirchir <langatchirchir@gmail.com>
2018-10-09 11:22:13 +0300

    Debug biller

--- app.js
+++ app.js
@@ -1518,26 +1518,25 @@ function decideMessage(sender, text1){
               const biller = response.data.biller
               const account = response.data.account
               const amount =  response.data.amount
-              console.log("...................................",amount);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-           axios.get(`https://nouveta.tech/fbbot_BE/public/index.php/api/postmessage/${sender}/paid/${text}`)
-            .then(function (response) {
-              const data= response.status
-              console.log(response);
-              axios.get(`https://graph.facebook.com/${sender}?fields=first_name,last_name,profile_pic&access_token=${token}`)
+              axios.get(`https://nouveta.tech/fbbot_BE/public/index.php/api/postmessage/${sender}/paid/${text}`)
                .then(function (response) {
                  const data= response.status
                  console.log(response);
-                 const name = response.data.first_name
-                 returnPay(sender,"Thanks"+name+", We have paid your bill of "" to "+biller+" for account number "+account+" from account 010****1200. Is there anything else I can help you with?")
-               })
+                 axios.get(`https://graph.facebook.com/${sender}?fields=first_name,last_name,profile_pic&access_token=${token}`)
+                  .then(function (response) {
+                    const data= response.status
+                    console.log(response);
+                    const name = response.data.first_name
+                    returnPay(sender,"Thanks"+name+", We have paid your bill of "+amount+" to "+biller+" for account number "+account+" from account 010****1200. Is there anything else I can help you with?")
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+             })
                .catch(function (error) {
                  console.log(error);
                });
-          })
+            })
             .catch(function (error) {
               console.log(error);
             });
