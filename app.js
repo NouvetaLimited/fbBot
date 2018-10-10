9b97ad2420f18001bf8130d05ccf159c5b50123d
Kibetchirchir <langatchirchir@gmail.com>
2018-10-10 08:11:41 +0300

    Added attahments end point

--- app.js
+++ app.js
@@ -56,14 +56,14 @@ app.post('/webhook/', function(req, res){
          let event = messaging_events[i];
         let sender = event.sender.id;
         // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.seeee",event);
+        if(event.message.attachments && event.message){
+          sendText(sender,"uuuwiiii")
+        }
 
         if(event.message && event.message.text){
             let text = event.message.text;
            //sendText(sender,"Text echo: " + text.substring(0,100))
            decideMessage(sender, text)
-           if(event.message.attachments){
-             sendText(sender,"uuuwiiii")
-           }
         }
         if(event.postback){
             let text = JSON.stringify(event.postback.payload)
